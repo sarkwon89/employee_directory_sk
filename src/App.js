3185dde9f124b9978c7b51a6ea5ebe24e16781e6
Sarah <sarkwon89@gmail.com>
2020-02-06 18:50:49 -0800

    created two columns

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 // import logo from './logo.svg';
-import UserList from './components/UserList'
-import Title from './components/Title'
-import API from './utils/API'
+import UserList from './components/UserList';
+import Title from './components/Title';
+import Search from './components/Search';
+import API from './utils/API';
+import './App.css';
 
 class App extends Component {
 
@@ -13,26 +15,32 @@ class App extends Component {
 
   componentDidMount() {
     API.randomuser()
-    .then(res => this.setState({ result: res.data.results }))
-    .catch(err => console.log(err));
+      .then(res => this.setState({ result: res.data.results }))
+      .catch(err => console.log(err));
   }
-  
+
   render() {
 
     return (
       <div className="container">
         <Title />
-        {this.state.result.map((element,index) => (
-          <UserList
-            key={index}
-            name={element.name.first + " " + element.name.last}
-            picture={element.picture.medium}
-            city = {element.location.city}
-            email = {element.email}
-            state = {element.location.state}
-
-          />
-        ))}
+        <div className="flex">
+          <div className="formbox">
+          <Search />
+          </div>
+          <div className="listbox">
+          {this.state.result.map((element, index) => (
+            <UserList
+              key={index}
+              name={element.name.first + " " + element.name.last}
+              picture={element.picture.medium}
+              city={element.location.city}
+              email={element.email}
+              state={element.location.state}
+            />
+          ))}
+          </div>
+        </div>
       </div>
     );
   }
